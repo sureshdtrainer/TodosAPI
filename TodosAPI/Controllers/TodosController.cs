5494b669623438a08ecda32182a1fd64bd1bc82c
sureshdtrainer <sureshdtrainer@gmail.com>
2025-01-22 15:09:36 +0530

    Implemented GetById, GetByTitle and Delete Todo functionality for Web API

--- TodosAPI/Controllers/TodosController.cs
+++ TodosAPI/Controllers/TodosController.cs
@@ -15,6 +15,29 @@ namespace TodosAPI.Controllers
             return TodosRespository.Todos;
         }
 
+        [HttpGet]
+        [Route("{id:int}")]
+        public ActionResult<Todo> GetTodoById(int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+        Todo todo = TodosRespository.Todos.Where(x => x.Id == id).FirstOrDefault();
+            if (todo == null)
+                return NotFound($"The todos with {id} not found");
+            return Ok(todo);
+        }
+
+        [HttpGet("{title}")]
+        public ActionResult<Todo> GetTodoByTitle(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+                return BadRequest();
+            Todo todo = TodosRespository.Todos.FirstOrDefault(x => x.Title == title);
+            if (todo == null)
+                return NotFound($"The todos with {title} not found");
+            return Ok(todo);
+        }
+
         [HttpPost]
         public ActionResult<Todo> CreateTodo(Todo todo)
         {
@@ -22,6 +45,17 @@ namespace TodosAPI.Controllers
             todo.Id = newId;
             TodosRespository.Todos.Add(todo);
             return Ok(todo);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<bool> DeleteTodoById(int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+            Todo todo = TodosRespository.Todos.Where(x => x.Id == id).FirstOrDefault();
+            if (todo == null)
+                return NotFound($"The todos with {id} not found");
+            return Ok(TodosRespository.Todos.Remove(todo));
         }
     }
 }
