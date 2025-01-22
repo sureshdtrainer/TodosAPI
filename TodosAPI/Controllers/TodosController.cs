5d2979b9a50ca91226ac47c72b1cdd8daef0a6e6
suresdtrainer <suresh.d.trainer@gmail.com>
2025-01-22 12:42:02 +0530

    Added POST/Create todo method

--- TodosAPI/Controllers/TodosController.cs
+++ TodosAPI/Controllers/TodosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using TodosAPI.Models;
 using TodosAPI.Repositories;
 
@@ -12,6 +13,15 @@ namespace TodosAPI.Controllers
         public List<Todo> GetAllTodos()
         {
             return TodosRespository.Todos;
+        }
+
+        [HttpPost]
+        public ActionResult<Todo> CreateTodo(Todo todo)
+        {
+            int newId = TodosRespository.Todos.LastOrDefault().Id + 1;
+            todo.Id = newId;
+            TodosRespository.Todos.Add(todo);
+            return Ok(todo);
         }
     }
 }
